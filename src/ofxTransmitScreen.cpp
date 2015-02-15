69d56d449a86cba3c85557edece436a4527648e9
Savant_Cat <scanf@me.com>
2015-02-15 14:32:42 +0900

    Unityにテクスチャとして転送確認 サイズが800x800以上になると表示が狂う時がある

---
+++ src/ofxTransmitScreen.cpp
@@ -0,0 +1,67 @@
+#include "ofxTransmitScreen.h"
+
+void ofxTransmitScreen::setup(string host,int port){
+	sender.setup(host, port);
+
+	ScreenBuffer.allocate(ofGetWidth(), ofGetHeight(), OF_IMAGE_COLOR);
+	message.setAddress("/oF/ScreenBuffer/");
+
+	ready = true;
+	quality = 100;
+	width   = ofGetWidth();
+	height  = ofGetHeight();
+}
+
+void ofxTransmitScreen::setOptions(int quality){
+	ofxTransmitScreen::quality = quality;
+}
+
+void ofxTransmitScreen::setOptions(int width,int heigh,int quality){
+	ofxTransmitScreen::width  = width;
+	ofxTransmitScreen::height = heigh;
+}
+
+void ofxTransmitScreen::get(){
+	//Get Screen img
+	if(sendReady){
+		ScreenBuffer.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		sendReady = false;
+	}
+}
+
+void ofxTransmitScreen::send(){
+	if(ready){
+		message.clear();
+
+		//optimization image
+		tmpBuffer.clone(ScreenBuffer);
+		tmpBuffer.resize(width,height);
+		tmpBuffer.rotate90(2);	//Unityではテクスチャが上下反転するための対策
+
+		//Makeing Jpg binary
+		compressed = turbo.compress(tmpBuffer,quality,&size);
+		ofBuffer data((char *)compressed,size);
+	
+		//Makeing OSC message
+		message.addIntArg(width); //Width  size
+		message.addIntArg(height);//Height size
+		message.addIntArg(size);  //Binary size 
+		message.addBlobArg(data); //Binary data 
+
+		//Send message
+		sender.sendMessage(message);
+
+		sendReady = true;
+	}else{
+		cout << "error: Did you setup?" << endl;
+	}
+}
+
+void ofxTransmitScreen::threadedFunction(){
+    while(isThreadRunning())
+    {
+
+		send();
+        //sleep(1);
+    }
+}
